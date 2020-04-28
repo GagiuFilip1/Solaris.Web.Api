8c5d118663a44da4c07f2e6186ace17b26fdc235
Filip <gagiu.filip@gmail.com>
2020-04-28 17:49:32 +0300

    SLR-1 Project Initialization

---
+++ Solaris.Web.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solaris.Web.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
